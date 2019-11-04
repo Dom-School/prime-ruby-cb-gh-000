b0bde4705bbb46a495dc82e7592466b6234eb985
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-11-04 14:49:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  return true if num == 2
+  count = 2
+  is_prime = false
+  while count < num
+    if num % count == 0
+      is_prime = false
+    end
+    count += 1
+  end
+  return is_prime
+end
